63a4822ff88ebfd2cc4e7046f6ac515341aa0065
niallmca <mcandrewniall@gmail.com>
2018-04-08 19:37:10 +0100

    Update BasicOTA_Hotpress.ino

--- BasicOTA_Hotpress/BasicOTA_Hotpress.ino
+++ BasicOTA_Hotpress/BasicOTA_Hotpress.ino
@@ -8,7 +8,7 @@ IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 const char* ssid = "TP-LINK_A005D1";
-const char* password = "137KincoraRoad";
+const char* password = "?????????";
 
 void setup() {
   Serial.begin(115200);
